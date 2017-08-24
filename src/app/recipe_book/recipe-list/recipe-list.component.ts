b7e60a6c123a7b353e59922e76025f6993efb67c
Matt Robinson <mthw.wm.robinson@gmail.com>
2017-08-23 21:33:30 -0400

    added recipe list

--- src/app/recipe_book/recipe-list/recipe-list.component.ts
+++ src/app/recipe_book/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg'),
     new Recipe('A Test Recipe', 'This is a test', 'https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg')
   ];
 
