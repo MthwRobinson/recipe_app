35cdeffb5872cf866a8578642d884ca7cbeb5895
Matt Robinson <mthw.wm.robinson@gmail.com>
2017-08-23 21:22:07 -0400

    added recipe model

--- src/app/recipe_book/recipe-list/recipe-list.component.ts
+++ src/app/recipe_book/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg')
+  ];
 
   constructor() { }
 
