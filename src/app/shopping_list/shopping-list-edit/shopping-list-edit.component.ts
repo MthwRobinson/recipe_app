972d64a18db27301ada6de4679a3aaf821363534
Matt Robinson <mthw.wm.robinson@gmail.com>
2017-09-01 22:27:46 -0400

    added edit list

--- src/app/shopping_list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping_list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  nameInput: string;
+  amountInput: number;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem(nameInput, amountInput){
+    const newIngredient = new Ingredient(
+      nameInput.value, 
+      amountInput.value
+    );
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
