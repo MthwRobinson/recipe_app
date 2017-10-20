999874d1409b5b2c252a264c5939ba6a4350b0c4
Matt Robinson <mthw.wm.robinson@gmail.com>
2017-10-20 17:47:02 -0400

    added recipe service to recipe book

--- src/app/recipe_book/recipe-book.component.ts
+++ src/app/recipe_book/recipe-book.component.ts
@@ -12,9 +12,14 @@ import { RecipeService } from './recipe.service';
 export class RecipeBookComponent implements OnInit {
   selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipeService.recipeSelected.subscribe(
+      (recipe: Recipe) => {
+        this.selectedRecipe = recipe;
+      }
+    );
   }
 
 }
--- src/app/recipe_book/recipe.service.ts
+++ src/app/recipe_book/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from '@angular/core';
+
 import { Recipe } from './recipe.model';
 
 export class RecipeService{
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('Bamboo!', 'This is a test', 'https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg'),
     new Recipe('More Baboo!', 'This is a test', 'https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg')
