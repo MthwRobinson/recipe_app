1d175ad1c5734374b739ffecc359617eda1f50c9
Matt Robinson <mthw.wm.robinson@gmail.com>
2017-10-20 17:28:58 -0400

    added recipe service to recipe lists

--- src/app/recipe_book/recipe-list/recipe-list.component.ts
+++ src/app/recipe_book/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,14 +10,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Bamboo!', 'This is a test', 'https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg'),
-    new Recipe('More Baboo!', 'This is a test', 'https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe){
--- src/app/recipe_book/recipe.service.ts
+++ src/app/recipe_book/recipe.service.ts
@@ -1,3 +1,14 @@
+import { Recipe } from './recipe.model';
+
 export class RecipeService{
+  private recipes: Recipe[] = [
+    new Recipe('Bamboo!', 'This is a test', 'https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg'),
+    new Recipe('More Baboo!', 'This is a test', 'https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg')
+  ];
+
+  getRecipes() {
+    // .slice() returns a copy of the array
+    return this.recipes.slice();
+  }
 
 }
