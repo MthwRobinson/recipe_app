dd2870d88b60e56b804551bed0925265547033ac
Matt Robinson <mthw.wm.robinson@gmail.com>
2017-10-22 16:50:30 -0400

    added ingredients to recipe

--- src/app/recipe_book/recipe.service.ts
+++ src/app/recipe_book/recipe.service.ts
@@ -1,13 +1,30 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService{
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Squawk! Bacon!', 'This is a test', 'https://github.com/MthwRobinson/recipe_app/blob/master/src/app/shared/jabber.jpg?raw=true'),
-    new Recipe('Bamboo!', 'This is a test', 'https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg')
+    new Recipe(
+      'Squawk! Bacon!', 
+      'This is a test', 
+      'https://github.com/MthwRobinson/recipe_app/blob/master/src/app/shared/jabber.jpg?raw=true',
+      [
+        new Ingredient('Fake Bacon', 3),
+        new Ingredient('Hungry Parrot', 1)
+      ]
+    ),
+    new Recipe(
+      'Bamboo!', 
+      'This is a test', 
+      'https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg',
+      [
+        new Ingredient('Bamboo!', 14),
+        new Ingredient('Hungry panda', 2)
+      ]
+    )
   ];
 
   getRecipes() {
