5bcaf9fed7d3b44c1a89b523339493f7ad2851a2
Matt Robinson <mthw.wm.robinson@gmail.com>
2017-10-22 16:37:12 -0400

    made ingredients service work

--- src/app/recipe_book/recipe.service.ts
+++ src/app/recipe_book/recipe.service.ts
@@ -6,8 +6,8 @@ export class RecipeService{
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Bamboo!', 'This is a test', 'https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg'),
-    new Recipe('More Baboo!', 'This is a test', 'https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg')
+    new Recipe('Squawk! Bacon!', 'This is a test', 'https://github.com/MthwRobinson/recipe_app/blob/master/src/app/shared/jabber.jpg?raw=true'),
+    new Recipe('Bamboo!', 'This is a test', 'https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg')
   ];
 
   getRecipes() {
